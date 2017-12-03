9f59d9b21da654f8709285b8596ad5ba39b191c3
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-12-03 01:18:46 -0600

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Ryan Wittrup on 12/3/17.
+//  Copyright © 2017 Ryan Wittrup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
